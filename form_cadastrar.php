56bbec2513f3c81ad2e9544cd82276934ea19175
PedroGimenes <pedro.gimenes@etec.sp.gov.br>
2025-08-15 17:11:32 -0300

    save

---
+++ form_cadastrar.php
@@ -0,0 +1,24 @@
+<?php
+   include 'cabecalho.php'
+?>
+<body>
+    <div class="container ">
+        <form>
+            <div class="mb-3">
+                <input type="text" name="produto" class="form-control" placeholder="Digite o nome do Produto">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control" placeholder="Digite o preço do Produto">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="text" class="form-control" placeholder="Digite a quantidade">
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+       </form>
+    </div>     
+
+
+<?php
+   include 'pontape.php'
+?>
+</html>
