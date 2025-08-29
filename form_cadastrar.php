7bd96c005a368dcc9ea2fada35cef5b5f783017f
PedroGimenes <pedro.gimenes@etec.sp.gov.br>
2025-08-29 18:17:38 -0300

    form_atualizar

--- form_cadastrar.php
+++ form_cadastrar.php
@@ -11,7 +11,7 @@
             <div class="mb-3">
                 <input type="text" name="preco" class="form-control" placeholder="Digite o preço do Produto">
             </div>
-            <div class="mb-3 form-check">
+            <div class="mb-3">
                 <input type="text" name="quantidade" class="form-control" placeholder="Digite a quantidade">
             </div>
             <button type="submit" class="btn btn-primary">Cadastrar</button>
