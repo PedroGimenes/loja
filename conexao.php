e55398b6e21c290465dcdb01b1310caee7f49666
PedroGimenes <pedro.gimenes@etec.sp.gov.br>
2025-09-19 18:21:42 -0300

    arrumando o banco de dados?

--- conexao.php
+++ conexao.php
@@ -3,6 +3,15 @@
     $dbname = 'loja';
     $user = 'root';
     $pass = '';
+
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+    $options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
         // Habilita erros do PDO
